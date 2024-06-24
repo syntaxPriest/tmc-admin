2d95c1e41633d51d224c33fbf36a8c3a6eef6f50
Daniel Adewale <adewaled01@gmail.com>
2024-06-24 18:03:00 +0100

    Update singleTransaction.tsx

--- src/components/dashboard/transactionComps/singleTransaction.tsx
+++ src/components/dashboard/transactionComps/singleTransaction.tsx
@@ -126,7 +126,7 @@ const TransactionInfo = () => {
                                     className='w-[80%]'
                                 >
                                     <Typography 
-                                        text={commaNumber(`${transactionsState?.data?.amount}`)}
+                                        text={commaNumber(`₦${transactionsState?.data?.amount}`)}
                                         color='#091525'
                                         fontWeight={700}
                                         fontSize='20px'
