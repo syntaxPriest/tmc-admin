96325c471ad14723f8b739078edaa703dc05307b
Daniel Adewale <adewaled01@gmail.com>
2024-07-12 10:18:09 +0100

    Update single.tsx

--- src/components/dashboard/membersComps/single.tsx
+++ src/components/dashboard/membersComps/single.tsx
@@ -37,6 +37,7 @@ import { getCdnLink } from '../../../utils/imageParser';
 import { membershipTypeList } from '../modals/inviteMembers';
 
 interface userStateProps {
+    subscription_start_date: any;
     data: User,
     wallet: any,
     transactions: any,
@@ -925,7 +926,7 @@ const MemberProfile = () => {
                                                         <div className="h-[4rem] border-r"></div>
                                                         <div className='text-center'>
                                                             <p className="font-[400] text-[11px] text-[#898579]">Next Subscription</p>
-                                                            <h3 className="font-[500] pt-1 text-[13px]">{userState?.subscriptions && userState?.subscriptions.length > 0 ? `${moment(userState?.subscriptions[0].created_at).format('LL').split(",")[0]}, ${new Date(userState?.subscriptions[0].created_at).getFullYear() + 1}` : "---"}</h3>
+                                                            <h3 className="font-[500] pt-1 text-[13px]">{userState?.subscription_start_date || userState?.subscriptions && userState?.subscriptions.length > 0 ? `${moment(userState?.subscriptions[0].created_at).format('LL').split(",")[0]}, ${new Date(userState?.subscriptions[0].created_at).getFullYear() + 1}` : "---"}</h3>
                                                         </div>
                                                     </div>
                                                 </div>
